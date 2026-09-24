0f89f858c8280aa02d41cb822689dd1bc809500b
Lord Lethris <lord.lethris@googlemail.com>
2026-09-24 21:08:21 +0100

    Gen 3: add render intent package

---
+++ gen3/render/__init__.py
@@ -0,0 +1,6 @@
+"""Render intent definitions for Gen 3 downstream generators.
+
+Render intent is presentation state, not Character DNA. It describes what a
+downstream image generator should show while leaving the character itself
+unchanged.
+"""
